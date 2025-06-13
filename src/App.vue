6840e925fc840ece0afc2bdb13d0a683c4c03127
zhiwarr <zhiwar04@gmail.com>
2025-06-13 19:34:51 +0300

    native js error handling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-const { activeError } = storeToRefs(useErrorStore())
+const errorStore = useErrorStore()
+
+onErrorCaptured((error) => {
+  errorStore.setError({ error: error })
+})
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+    <AppErrorPage v-if="errorStore.activeError" />
     <RouterView v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name" />
--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+// app.config.errorHandler((error) => {
+// ----
+// })
+
 app.mount('#app')
